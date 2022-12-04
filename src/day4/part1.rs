5fbb3bb23336ff268bfccda5c3598925d12c800c
lectromoe <contact@lectro.moe>
2022-12-04 10:30:37 +0300

    day 4

---
+++ src/day4/part1.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+#[rustfmt::skip]
+fn main() {
+    let input = fs::read_to_string("src/day4/input.txt").unwrap();
+    let mut amount = 0;
+
+    input.lines().for_each(|line| {
+        let mut nums = line.split(',')
+            .flat_map(|range| range.split('-').collect::<Vec<_>>());
+
+        let first_start = nums.next().unwrap().parse::<i32>().unwrap();
+        let first_end = nums.next().unwrap().parse::<i32>().unwrap();
+        let second_start = nums.next().unwrap().parse::<i32>().unwrap();
+        let second_end = nums.next().unwrap().parse::<i32>().unwrap();
+
+        if (first_start <= second_start && first_end >= second_end)
+        || (first_start >= second_start && first_end <= second_end)
+        {
+            amount += 1;
+        }
+    });
+
+    println!("{amount}");
+}
